679f918d9292b105f4629c95e166a9118f412048
Code Madness <codemadness@hotmail.com>
2014-12-06 12:49:18 +0000

    angular rails

---
+++ app/assets/javascripts/ng-app/app.js
@@ -0,0 +1,14 @@
+angular
+    .module('AngularRails', [
+        'ngRoute',
+        'templates'
+    ])
+
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'home.html',
+                controller: 'HomeCtrl'
+            });
+        $locationProvider.html5Mode(true);
+    });
